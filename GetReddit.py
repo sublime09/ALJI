b9b16f9a233e16d6b29522f9a38eca4683edd9cf
Patrick Sullivan <sublime@vt.edu>
2018-05-13 15:39:35 -0400

    beginning skeleton

---
+++ GetReddit.py
@@ -0,0 +1,13 @@
+'''
+Get Reddit part of ALJI project
+'''
+
+from ProgressBar import ProgressBar
+
+
+def main():
+	pass
+
+
+if __name__ == '__main__':
+	main()
---
+++ JournalSource.py
@@ -0,0 +1,17 @@
+'''
+ALJI project Journal source
+'''
+
+def main():
+	pass
+
+class JournalSource:
+
+	def __init__(self):
+		self.url = None
+		self.date = None
+		self.statements = None
+
+
+if __name__ == '__main__':
+	main()

e00ad5196443fe4b55683dcce5891a04d0d091a6
Patrick Sullivan <sublime@vt.edu>
2018-05-13 15:38:57 -0400

    main central line

---
+++ ALJI.py
@@ -0,0 +1,14 @@
+''' ALJI
+The Active Listening Journal Interaction Project
+By Patrick Sullivan
+'''
+
+
+def main():
+	print("Running Entire ALJI Project...")
+	# progBar = ProgressBar(range(20))
+
+
+
+if __name__ == '__main__':
+	main()

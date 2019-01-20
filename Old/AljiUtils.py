a4abdddd38fa43189519296e595208ee428f2cc3
Patrick <sublime@vt.edu>
2019-01-20 17:23:02 -0500

    moves archived code to old folder

--- Old/AljiUtils.py
+++ Old/AljiUtils.py
@@ -8,3 +8,13 @@ def millis():
 
 def stringy(*args):
 	return ' '.join([str(s) for s in args])
+
+def printDict(d, level=0):
+    indent = '\t'*level
+    for k, v in d.items():
+        if isinstance(v, dict):
+            print(indent, k, ":{")
+            printDict(v, level=level+1)
+            print(indent, "}")
+        else:
+            print(indent, k, ":", v)
